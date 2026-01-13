e043c7f504d86d028ed7c136d052e415c6746825
EduardoAndradeSantos <eduardospore@live.com>
2026-01-12 22:05:12 -0300

    feat: teste de primeira pagina

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  template: `
+    <h1>Gestão Financeira</h1>
+    <p>Frontend Angular funcionando 🚀</p>
+  `,
+})
+export class HomeComponent {}
